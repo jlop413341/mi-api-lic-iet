7867957fcbf8154d69c47e55689d015260be82ca
jlop413341 <joaquinlopez56t@gmail.com>
2024-11-05 14:41:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,27 +15,31 @@ app.use(express.json());
 //const serviceAccount = require('./config/licenciasiet-firebase-adminsdk-lx2et-7b021ea963.json');
 //const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS_JSON);
 
+/ Configurar Firebase Admin con las credenciales separadas
+const serviceAccount = {
+    type: 'service_account',
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Reemplazar \n por saltos de línea
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+};
 
-let serviceAccount;
-if (process.env.FIREBASE_SERVICE_ACCOUNT) {
-    try {
-        serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-        console.log("Firebase credentials loaded successfully.");
-    } catch (error) {
-        console.error("Error parsing Firebase credentials JSON:", error);
-    }
-} else {
-    console.error("FIREBASE_SERVICE_ACCOUNT environment variable is not set.");
-}
 
 // Inicia Firebase solo si las credenciales fueron cargadas correctamente
 if (serviceAccount) {
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
     });
+    console.log("Firebase credentials loaded successfully.");
 } else {
     console.error("Failed to initialize Firebase Admin SDK due to missing credentials.");
 }
+
 
 
 
