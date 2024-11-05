60ea5264d35d0f0b99304d93e28d55932c37771a
jlop413341 <joaquinlopez56t@gmail.com>
2024-11-05 12:49:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-// Importa Express
 const express = require('express');
 const app = express();
+
+app.use(express.json());
 
 // Ruta básica de prueba
 app.get('/', (req, res) => {
     res.send('API en línea');
 });
 
-// Exporta `app` para que Vercel lo maneje sin `app.listen`
+// Ruta para verificar la licencia
+app.post('/verificar-licencia', (req, res) => {
+    const { licencia, ip } = req.body;
+    res.json({
+        mensaje: 'Verificación de licencia recibida',
+        licencia: licencia,
+        ip: ip
+    });
+});
+
 module.exports = app;
